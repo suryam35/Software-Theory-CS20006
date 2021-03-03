099b7355b944f6bc0dec436cbd656c2e777d7e39
Suryam Arnav Kalra <suryamkalra35@gmail.com>
2021-03-03 22:01:31 +0530

    Create Main.cpp

---
+++ Assignment-2/Main.cpp
@@ -0,0 +1,14 @@
+// File: Main.cxx
+// Contains: int main()
+/************ C++ Headers ************************************/
+#include <iostream>
+using namespace std;
+/************ PROJECT Headers ********************************/
+#include "Fraction.h"
+void TestFraction();
+
+int main() {
+    TestFraction();
+    return 0;
+}
+// End-of-File: Main.cxx
